b26b1cc67ada4456554dc1aedd29c8753e323ed7
bala srinivas <34137043+balasrinivas41@users.noreply.github.com>
2018-04-13 23:25:57 +0530

    Add files via upload
    modified files

---
+++ Kh.java
@@ -0,0 +1,48 @@
+package DataObjects;
+
+public class Kh {
+	public static void main(String args[])
+	{
+		mains a=new mains();
+		a.maing();
+		//Adminpage aa=new Adminpage();
+		//aa.maing();
+		//createUser cu=new createUser();
+		//cu.maing();
+		
+		//createObject co=new createObject();
+		//co.maing();
+		//changePassword p=new changePassword();
+	   // p.maing();
+		//Userpage up=new Userpage();
+		//up.maing("usr1003","pwd2003");
+		
+		//up.maing("usr1001","pwd2001");
+		//up.maing("usr1004","pwd2004");
+		//up.submitbActionPerformeds();
+		//read rr=new read();
+		
+		//rr.maing("l.txt");
+		
+	//	write ww=new write();
+		//ww.maing("C:/Users/bala sirnevas/Desktop/l.txt");
+		
+		//objectCreatedMessage oc=new objectCreatedMessage();
+		//oc.maing();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
